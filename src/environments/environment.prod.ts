cd9e2d62d709163fa791cf8575418db818254d69
nicolaspantano <nicolaspantano_757@hotmail.com>
2021-04-08 08:45:20 -0300

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCsOjuR7rg6F2YLeAVbqAP8zahq5CLHNmk",
+    authDomain: "sala-de-juegos-8bb74.firebaseapp.com",
+    databaseURL: "https://sala-de-juegos-8bb74-default-rtdb.firebaseio.com",
+    projectId: "sala-de-juegos-8bb74",
+    storageBucket: "sala-de-juegos-8bb74.appspot.com",
+    messagingSenderId: "973768523085",
+    appId: "1:973768523085:web:5ecfa731410a7f4279cb04"
+  }
 };
